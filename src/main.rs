c1c6970f9e0ecc2fd5ac7fa38e867615eba9c2f1
Santiago Cingolani <santiago.cingolani@esrlabs.com>
2022-12-21 15:28:13 +0100

    We got it right! :)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-use std::collections::HashMap;
+use std::{
+    collections::{hash_map::DefaultHasher, HashMap, HashSet},
+    hash::Hash,
+    sync::{atomic::AtomicUsize, Mutex},
+};
+
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
+use regex::Regex;
+
+const INPUT: &str = include_str!("../input.txt");
 
 const MAX_STEPS: usize = 23;
 
-#[derive(Eq, Hash, PartialEq, Clone, Copy, Debug)]
+#[derive(Eq, Hash, PartialEq, Clone, Copy, Debug, PartialOrd, Ord)]
 enum Resource {
     Ore,
     Clay,
@@ -12,7 +23,7 @@ enum Resource {
 
 type Inventory = HashMap<Resource, usize>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct State {
     robots: Inventory,
     resources: Inventory,
@@ -46,12 +57,45 @@ impl Default for State {
     }
 }
 
+impl Hash for State {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        let mut robots: Vec<_> = self.robots.iter().collect();
+        robots.sort_by_key(|(res, _)| **res);
+        let mut resources: Vec<_> = self.resources.iter().collect();
+        resources.sort_by_key(|(res, _)| **res);
+        robots.hash(state);
+        resources.hash(state);
+    }
+}
+
+impl PartialEq for State {
+    fn eq(&self, other: &Self) -> bool {
+        if self.robots.len() != other.robots.len() {
+            false
+        } else {
+            let mut result = true;
+            for (res, count) in self.resources.iter() {
+                result = result && (other.resources[res] == *count);
+            }
+            for (res, count) in self.robots.iter() {
+                result = result && (other.robots[res] == *count);
+            }
+            result
+        }
+    }
+}
+impl Eq for State {}
+
+#[derive(Debug)]
 struct Recipe {
     costs: Inventory,
     robot_type: Resource,
 }
 
+
+#[derive(Debug)]
 struct Blueprint {
+    id: usize,
     recipes: Vec<Recipe>,
 }
 
@@ -84,36 +128,61 @@ impl Blueprint {
     }
 }
 
-fn parse_blueprint() -> Blueprint {
-    // Each ore robot costs 4 ore.
-    // Each clay robot costs 2 ore.
-    // Each obsidian robot costs 3 ore and 14 clay.
-    // Each geode robot costs 2 ore and 7 obsidian.
-    Blueprint {
-        recipes: vec![
-            Recipe {
-                costs: HashMap::from_iter(vec![(Resource::Ore, 4)]),
-                robot_type: Resource::Ore,
-            },
-            Recipe {
-                costs: HashMap::from_iter(vec![(Resource::Ore, 2)]),
-                robot_type: Resource::Clay,
-            },
-            Recipe {
-                costs: HashMap::from_iter(vec![(Resource::Ore, 3), (Resource::Clay, 14)]),
-                robot_type: Resource::Obsidian,
-            },
-            Recipe {
-                costs: HashMap::from_iter(vec![(Resource::Ore, 2), (Resource::Obsidian, 7)]),
-                robot_type: Resource::Geode,
-            },
-        ],
+impl From<&str> for Blueprint {
+    fn from(input: &str) -> Self {
+        // Blueprint 1: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 3 ore and 10 clay. Each geode robot costs 2 ore and 7 obsidian.
+        let re = Regex::new("Blueprint ([0-9]+): Each ore robot costs ([0-9]+) ore. Each clay robot costs ([0-9]+) ore. Each obsidian robot costs ([0-9]+) ore and ([0-9]+) clay. Each geode robot costs ([0-9]+) ore and ([0-9]+) obsidian.").expect("Invalid regex");
+        let captures: Vec<usize> = re
+            .captures(input)
+            .expect("Failed to parse with regex")
+            .iter()
+            .skip(1) // skip capture group 0
+            .map(|capture| {
+                capture
+                    .expect("Missing capture")
+                    .as_str()
+                    .parse::<usize>()
+                    .unwrap()
+            })
+            .collect();
+
+        Blueprint {
+            id: captures[0],
+            recipes: vec![
+                Recipe {
+                    costs: HashMap::from_iter(vec![(Resource::Ore, captures[1])]),
+                    robot_type: Resource::Ore,
+                },
+                Recipe {
+                    costs: HashMap::from_iter(vec![(Resource::Ore, captures[2])]),
+                    robot_type: Resource::Clay,
+                },
+                Recipe {
+                    costs: HashMap::from_iter(vec![
+                        (Resource::Ore, captures[3]),
+                        (Resource::Clay, captures[4]),
+                    ]),
+                    robot_type: Resource::Obsidian,
+                },
+                Recipe {
+                    costs: HashMap::from_iter(vec![
+                        (Resource::Ore, captures[5]),
+                        (Resource::Obsidian, captures[6]),
+                    ]),
+                    robot_type: Resource::Geode,
+                },
+            ],
+        }
     }
+}
+
+fn parse_blueprint() -> Blueprint {
+    Blueprint::from("Blueprint 0: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.")
 }
 
 fn maximum_possible_geodes(state: &State) -> usize {
     let remaining_steps: usize = MAX_STEPS - state.step;
-    let n = remaining_steps;
+    let n = remaining_steps + 1;
 
     let max_possible = (((n + 1) * n) / 2)
         + (state.robots.get(&Resource::Geode).unwrap_or(&0) * n)
@@ -125,23 +194,31 @@ fn traverse_depth_first(
     mut state: State,
     current_max: usize,
     blueprint: &Blueprint,
+    metastate: &mut MetaState,
 ) -> Option<usize> {
     let decisions = blueprint.possible(&state.resources);
 
-    // println!("{:?}", state);
-
     if maximum_possible_geodes(&state) < current_max {
+        metastate.pruned_states += 1;
         return None;
     }
 
     state.tick();
 
+    if let Some(similar_state) = metastate.explored_states.get(&state) {
+        if similar_state.step <= state.step {
+            metastate.duplicated_states += 1;
+            return None;
+        }
+    }
+
     if state.step > MAX_STEPS {
         let geodes = state.resources[&Resource::Geode];
-        println!("{:?}", geodes);
-        if geodes > current_max {
-            println!("New Max! {:?}", state);
-        }
+        // // println!("{:?}", geodes);
+        // if geodes > current_max {
+        //     println!("New Max! {:?}", state);
+        // }
+        metastate.full_traversals += 1;
         return Some(geodes);
     }
 
@@ -168,20 +245,62 @@ fn traverse_depth_first(
             }
         })
         .collect();
-    possible_new_states.push(state);
+    possible_new_states.push(state.clone());
+
+    // possible_new_states.shuffle(&mut thread_rng());
 
     let mut new_max = current_max;
-    for mut new_state in possible_new_states {
-        let result = traverse_depth_first(new_state, new_max, blueprint);
+    for new_state in possible_new_states {
+        let result = traverse_depth_first(new_state, new_max, blueprint, metastate);
         new_max = new_max.max(result.unwrap_or_default());
     }
+
+    if new_max == current_max {
+        if metastate.explored_states.contains(&state) {
+            {
+                let existing = metastate.explored_states.get(&state).unwrap();
+                assert!(existing.step > state.step);
+            }
+            metastate.explored_states.remove(&state);
+        } else {
+            metastate.explored_states.insert(state);
+        }
+    }
+
     Some(new_max)
+}
+
+#[derive(Default)]
+struct MetaState
+{
+    explored_states: HashSet<State>,
+    duplicated_states: usize,
+    pruned_states: usize,
+    full_traversals: usize,
 }
 
 fn main() {
     println!("Hello elves!");
-    let blueprint = parse_blueprint();
 
-    let result = traverse_depth_first(State::default(), 0, &blueprint);
-    println!("{:?}", result);
+    let mut final_result: usize = 0;
+
+    for line in INPUT.lines()
+    {
+        let mut metastate = MetaState::default();
+
+        let blueprint = Blueprint::from(line);
+        println!("{:?}", blueprint);
+        let result = traverse_depth_first(State::default(), 0, &blueprint, &mut metastate);
+        println!("{:?}", result);
+        println!(
+            "Skipped because duplicate: {}; pruned: {}; full traversals: {}.",
+            metastate.duplicated_states,
+            metastate.pruned_states,
+            metastate.full_traversals
+        );
+        final_result += result.unwrap() * blueprint.id;
+    }
+
+    println!("Final result: {}", final_result);
+
 }
